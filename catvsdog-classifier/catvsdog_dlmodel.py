a090111580d090ced571a378f53b21ea344f2a51
A V BHARAT SREYUS <57716921+bharatsreyus@users.noreply.github.com>
2020-01-25 20:43:30 +0530

    Create catvsdog_dlmodel.py

---
+++ catvsdog-classifier/catvsdog_dlmodel.py
@@ -0,0 +1,33 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
+import pickle
+
+#pickle in features and labels
+pickle_in = open('X_catvsdog','rb')
+X = pickle.load(pickle_in)
+pickle_in = open('y_catvsdog','rb')
+y = pickle.load(pickle_in)
+
+X = X/255.0 #set the values in between 0 and 1
+
+model = Sequential()
+
+model.add(Conv2D(64, (3,3), input_shape = X.shape[1:]) )
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size = (2,2)))
+
+model.add(Conv2D(64, (3,3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size = (2,2)))
+
+model.add(Flatten())
+model.add(Dense(64))
+
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+model.compile(loss = 'binary_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
+
+model.fit(X, y, batch_size = 32, epochs=10, validation_split= 0.1)
+                                                                           
